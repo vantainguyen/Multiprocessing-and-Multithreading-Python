574f8a186fbee3579a6d93c706a669d14a2aa812
Van Tai Nguyen <s4612643@eait.uq.edu.au>
2021-09-21 12:41:41 +1000

    second commit

--- letter_count.py
+++ letter_count.py
@@ -11,9 +11,10 @@ from threading import Thread, Lock
 
 finished_count = 0
 
-def count_letters(url, frequency):
+def count_letters(url, frequency, mutex):
     response = urllib.request.urlopen(url)
     txt = str(response.read())
+    mutex.acquire()
     
     for l in txt:
         letter = l.lower()
@@ -24,9 +25,14 @@ def count_letters(url, frequency):
     global finished_count
     
     finished_count += 1
+    mutex.release()
+    
+
             
 def main():
     frequency = {}
+    
+    mutex = Lock()
     
     for c in "abcdefghijklmnopqrstuvwxyz":
         frequency[c] = 0
@@ -34,9 +40,14 @@ def main():
     start = time.time()
     
     for i in range(1000, 1020):
-        Thread(target=count_letters, args=(f"https://www.rfc-editor.org/rfc/rfc{i}.txt", frequency)).start()
+        Thread(target=count_letters, args=(f"https://www.rfc-editor.org/rfc/rfc{i}.txt", frequency, mutex)).start()
         
-    while finished_count < 20:
+        
+    while True:
+        mutex.acquire()
+        if finished_count == 20:
+            break
+        mutex.release()
         time.sleep(.5)
         
     end = time.time()
